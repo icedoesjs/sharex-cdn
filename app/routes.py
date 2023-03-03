1f45ef4bde704981b23503f5c89ca2208ea822d8
Jordan <40555230+icedoesjs@users.noreply.github.com>
2023-03-03 13:14:06 -0500

    Add C and RUST support

--- app/routes.py
+++ app/routes.py
@@ -115,7 +115,7 @@ def get_type(filename):
     images = ['jpg', 'png', 'jpeg', 'gif']
     file = ['txt']
     audio = ["wav", "mp3"]
-    code = ["py", "js", "php", "ts", "cpp", "html", "cs", "json", "css", "sql", "asm"]
+    code = ["py", "js", "php", "ts", "cpp", "html", "cs", "json", "css", "sql", "asm", "c", "rs"]
     video = ["mov", "mp4"]
     folder = ["zip", "rar", "7z"]
     extension = filename.split(".")[1]
